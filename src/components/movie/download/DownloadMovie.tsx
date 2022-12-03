d96d18fd57374a6697396b27eab55b1eca3fe6de
Ezekiel Victor <codeepoch@gmail.com>
2022-12-03 03:26:21 +0100

    fixed download issues

--- src/components/movie/download/DownloadMovie.tsx
+++ src/components/movie/download/DownloadMovie.tsx
@@ -13,7 +13,7 @@ const DownloadMovie = (props: DownloadMovieProps) => {
       className='flex items-center justify-center'>
       <div className='max-w-[95vw] rounded-md flex shadow-lg shadow-light-2/10 items-center justify-center relative overflow-hidden'>
         <IconButton onClick={close} className='absolute top-0 left-0' />
-        <a className='w-full flex flex-col items-center justify-center' href={torrentURI} download></a>
+        <a className='w-full overflow-auto flex flex-col items-center justify-center' href={torrentURI} download></a>
       </div>
     </motion.div>
   )
